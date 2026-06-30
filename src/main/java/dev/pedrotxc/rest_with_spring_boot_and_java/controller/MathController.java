098a86d6956f72c84301eaa3bea455da4f7f1d74
Pedro Henrique <pedrotxc@gmail.com>
2026-06-29 23:54:04 -0300

    Definindo a lógica do método /math/sum e os métodos de validação

---
+++ src/main/java/dev/pedrotxc/rest_with_spring_boot_and_java/controller/MathController.java
@@ -0,0 +1,41 @@
+package dev.pedrotxc.rest_with_spring_boot_and_java.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/math")
+public class MathController {
+
+    @RequestMapping("/sum/{numberOne}/{numberTwo}")
+    public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
+
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new IllegalArgumentException();
+        }
+
+        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+    }
+
+    private Double convertToDouble(String strNumber) {
+        if (strNumber == null || strNumber.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+
+        String number = strNumber.replace(",", ".");
+
+        return Double.parseDouble(number);
+    }
+
+    private boolean isNumeric(String strNumber) {
+        if (strNumber == null || strNumber.isEmpty()) {
+            return false;
+        }
+
+        String number = strNumber.replace(",", ".");
+
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+    }
+
+}
